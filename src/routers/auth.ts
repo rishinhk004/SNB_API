92319558c3841f3aaa7ed2386be97163f8cc3d52
rishinhk004 <rishirajsaha555@gmail.com>
2025-07-21 02:45:32 +0530

    fixes

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -3,7 +3,7 @@ import * as Controllers from "../controllers";
 
 const router = express.Router();
 
-router.post("/sihnUp", Controllers.Auth.create);
+router.post("/signUp", Controllers.Auth.create);
 router.post("/signIn", Controllers.Auth.Login);
 
 export default router;
